58bb82cf0a43245afa49e8d3236451a17d42b2ce
Daniel Piron <dannypiron@gmail.com>
2016-01-23 22:47:41 -0500

    Tests for no bumper character

--- test_makeprogress.py
+++ test_makeprogress.py
@@ -5,10 +5,17 @@ import unittest
 class TestRenderProgressBar(unittest.TestCase):
 
     def test_basic_progress_bar_render(self):
-        self.assertEqual('>---------', mp.render_bar(0, 10))
-        self.assertEqual('=====>----', mp.render_bar(5, 10))
+        self.assertEqual('----------', mp.render_bar(0, 10))
+        self.assertEqual('=====-----', mp.render_bar(5, 10))
         self.assertEqual('==========', mp.render_bar(10, 10))
         self.assertEqual('==========', mp.render_bar(20, 10))
+
+    def test_basic_progress_bar_render_no_bumper(self):
+        NULL = ''
+        self.assertEqual('>---------', mp.render_bar(0, 10, bumper_char=NULL))
+        self.assertEqual('=====>----', mp.render_bar(5, 10, bumper_char=NULL))
+        self.assertEqual('==========', mp.render_bar(10, 10, bumper_char=NULL))
+        self.assertEqual('==========', mp.render_bar(20, 10, bumper_char=NULL))
 
     def test_one_cell_progress_bar(self):
         self.assertEqual('>', mp.render_bar(0, 1))
@@ -18,6 +25,17 @@ class TestRenderProgressBar(unittest.TestCase):
     def test_zero_cell_progress_bar(self):
         self.assertEqual('', mp.render_bar(0, 0))
         self.assertEqual('', mp.render_bar(1, 0))
+
+    def test_one_cell_progress_bar_no_bumper(self):
+        NULL = ''
+        self.assertEqual('-', mp.render_bar(0, 1, bumper_char=NULL))
+        self.assertEqual('=', mp.render_bar(1, 1, bumper_char=NULL))
+        self.assertEqual('=', mp.render_bar(10, 1, bumper_char=NULL))
+
+    def test_zero_cell_progress_bar_no_bumper(self):
+        NULL = ''
+        self.assertEqual('', mp.render_bar(0, 0, bumper_char=NULL))
+        self.assertEqual('', mp.render_bar(1, 0, bumper_char=NULL))
 
 if __name__ == '__main__':
     unittest.main()
